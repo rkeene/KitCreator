7a6da948f94552048b8cc3ae39657b2d4a4e7048
rkeene <rkeene@noemail.net>
2010-09-26 04:37:52 +0000

    Added support for creating proper kitsh environment
    FossilOrigin-Name: 5996f7a35ff58af70f50dcb5c13ef7f1e884542a

--- build/pre.sh
+++ build/pre.sh
@@ -1,3 +1,17 @@
-#! /bin/sh
+#! /bin/bash
 
 ./kitcreator distclean
+
+KITSHROOTDIR="$(ls -1d kitsh/buildsrc/kitsh-*/)"
+export KITSHROOTDIR
+
+(
+	cd "${KITSHROOTDIR}" || exit 1
+
+	autoconf; autoheader
+	rm -rf autom4te.cache
+
+	./configure
+
+	make distclean
+)
