d4fc6ec8d1bf0278eba03d98ba2516e1fc898d25
rkeene <rkeene@noemail.net>
2010-09-28 22:03:23 +0000

    Added regression suite tests for small zip files
    FossilOrigin-Name: de59e55104edeffcc09046067ebb85b8eadd95c2

---
+++ build/test/tests/09-smallzip-nobase.tcl
@@ -0,0 +1,31 @@
+#! /usr/bin/env tclsh
+
+# Abort if we cant read the tmpfile path from the environment
+if {![info exists ::env(TMPFILE)]} {
+	exit 0
+}
+
+set tmpfile $::env(TMPFILE)
+
+vfs::zip::Mount $tmpfile $tmpfile
+
+set fd [open [file join $tmpfile main.tcl]]
+set data [read $fd]
+close $fd
+
+catch {
+	vfs::unmount $tmpfile
+}
+
+catch {
+	file delete -force -- $tmpfile
+}
+
+if {[string match "*Hello World*" $data]} {
+	exit 0
+}
+
+puts "Got:              $data"
+puts "Expected (match): *Hello World*"
+
+exit 1
