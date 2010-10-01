cb4efdddaed72fc5de6fc70a218b196278fb24a1
rkeene <rkeene@noemail.net>
2010-10-01 20:45:02 +0000

    Updated to include a "package ifneeded" script for Tk on Win32 in addition to other platforms, if Tk built statically.
    FossilOrigin-Name: d3373882b32e13067455cbc964afe86aea82cf0c

--- kitsh/buildsrc/kitsh-0.0/kitInit.c
+++ kitsh/buildsrc/kitsh-0.0/kitInit.c
@@ -170,11 +170,9 @@ static char *preInitCmd =
 	"}\n"
 	"uplevel #0 $s\n"
 #if defined(KIT_INCLUDES_TK) && defined(KIT_TK_VERSION)
-#  ifndef _WIN32
 	"package ifneeded Tk " KIT_TK_VERSION " {\n"
 		"load {} Tk\n"
 	"}\n"
-#  endif
 #endif
 #ifdef _WIN32
 	"catch {load {} dde}\n"
