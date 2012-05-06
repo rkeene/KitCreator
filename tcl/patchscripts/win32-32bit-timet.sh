8372b95194c57167bb72c474889c7ab51191f518
rkeene <rkeene@noemail.net>
2012-05-06 22:21:16 +0000

    Minor update to clean-up warnings
    FossilOrigin-Name: 1e19f573c8d62a0b332eba25973b2da0e86166a1

--- tcl/patchscripts/win32-32bit-timet.sh
+++ tcl/patchscripts/win32-32bit-timet.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
 (
+	echo '#ifndef _USE_32BIT_TIME_T'
 	echo '#define _USE_32BIT_TIME_T 1'
+	echo '#endif'
 	cat generic/tcl.h
 ) > generic/tcl.h.new
 cat generic/tcl.h.new > generic/tcl.h
