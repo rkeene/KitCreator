e451f332a01d24e90e07cf5853a040beaf9d3d6f
rkeene <rkeene@noemail.net>
2013-09-25 04:30:34 +0000

    Removed dead code
    FossilOrigin-Name: 4816a85fc11a92182bbe8bbbd75ac3d6a5c21f35

--- kitsh/buildsrc/kitsh-0.0/main.c
+++ kitsh/buildsrc/kitsh-0.0/main.c
@@ -4,10 +4,6 @@
 int TclKit_AppInit(Tcl_Interp *interp);
 
 int main(int argc, char **argv) {
-	Tcl_Interp *x;
-
-	x = Tcl_CreateInterp();
-
 	Tcl_Main(argc, argv, TclKit_AppInit);
 
 	/* If Tcl_Main() returns, something went wrong */
