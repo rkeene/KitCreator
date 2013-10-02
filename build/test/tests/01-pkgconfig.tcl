518f88e0234b38718a791dbc626b5ca571778cc0
rkeene <rkeene@noemail.net>
2013-10-02 11:45:12 +0000

    Updated check for 64bit platforms
    FossilOrigin-Name: 2de7bba382b6a44a4d6df13cd4298ecbcfa9ada5

--- build/test/tests/01-pkgconfig.tcl
+++ build/test/tests/01-pkgconfig.tcl
@@ -5,7 +5,15 @@ if {$tcl_version == "8.4"} {
 	exit 0
 }
 
-if {[tcl::pkgconfig get 64bit] == 0} {
+# Determine if we should be 64-bit or not
+set buildflags [split [lindex $argv 1] -]
+if {[lsearch -exact $buildflags "amd64"] != -1} {
+	set is64bit 1
+} else {
+	set is64bit 0
+}
+
+if {[tcl::pkgconfig get 64bit] == $is64bit} {
 	exit 0
 }
 
