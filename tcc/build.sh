6150f453f288c30fa93bb9d589b4190b80b4d315
rkeene <rkeene@noemail.net>
2014-04-27 04:17:16 +0000

    Started work on adding TclTCC package
    FossilOrigin-Name: 2a6f69ee57e3f7b2de7924dee48457f60408e4d6

---
+++ tcc/build.sh
@@ -0,0 +1,148 @@
+#! /usr/bin/env bash
+
+if [ ! -f 'build.sh' ]; then
+	echo 'ERROR: This script must be run from the directory it is in' >&2
+
+	exit 1
+fi
+if [ -z "${TCLVERS}" ]; then
+	echo 'ERROR: The TCLVERS environment variable is not set' >&2
+
+	exit 1
+fi
+
+TCLTCCVERS="0.4"
+SRC="src/tcltcc-${TCLTCCVERS}.zip"
+SRCURL="https://tcltcc.googlecode.com/files/tcltcc${TCLTCCVERS}.zip"
+BUILDDIR="$(pwd)/build/tcltcc-0.4"
+OUTDIR="$(pwd)/out"
+INSTDIR="$(pwd)/inst"
+PATCHDIR="$(pwd)/patches"
+export TCLTCCVERS SRC SRCURL BUILDDIR OUTDIR INSTDIR PATCHDIR
+
+# Set configure options for this sub-project
+LDFLAGS="${KC_TCLTCC_LDFLAGS}"
+CFLAGS="${KC_TCLTCC_CFLAGS}"
+CPPFLAGS="${KC_TCLTCC_CPPFLAGS}"
+LIBS="${KC_TCLTCC_LIBS}"
+export LDFLAGS CFLAGS CPPFLAGS LIBS
+
+rm -rf 'build' 'out' 'inst'
+mkdir 'build' 'out' 'inst' || exit 1
+
+TCL_VERSION="unknown"
+if [ -f "${TCLCONFIGDIR}/tclConfig.sh" ]; then
+        source "${TCLCONFIGDIR}/tclConfig.sh"
+fi
+export TCL_VERSION
+
+if [ ! -f "${SRC}" ]; then
+	mkdir 'src' >/dev/null 2>/dev/null
+
+	if [ ! -d 'buildsrc' ]; then
+		rm -f "${SRC}.tmp"
+		wget -O "${SRC}.tmp" "${SRCURL}" || exit 1
+		mv "${SRC}.tmp" "${SRC}"
+	fi
+fi
+
+(
+	cd 'build' || exit 1
+
+	if [ ! -d '../buildsrc' ]; then
+		mkdir "${BUILDDIR}" || exit 1
+		(
+			cd "${BUILDDIR}" || exit 1
+			unzip "../../${SRC}"
+		)
+	else    
+		cp -rp ../buildsrc/* './'
+	fi
+
+	# Apply required patches
+	cd "${BUILDDIR}" || exit 1
+	for patch in "${PATCHDIR}/all"/tcltcc-${TCLTCCVERS}-*.diff "${PATCHDIR}/${TCL_VERSION}"/tcltcc-${TCLTCCVERS}-*.diff; do
+		if [ ! -f "${patch}" ]; then
+			continue
+		fi
+
+		echo "Applying: ${patch}"
+		${PATCH:-patch} -p1 < "${patch}"
+	done
+
+	cd "${BUILDDIR}" || exit 1
+
+	# Try to build as a shared object if requested
+	if [ "${STATICTCLTCC}" = "0" ]; then
+		tryopts="--enable-shared --disable-shared"
+	elif [ "${STATICTCLTCC}" = "-1" ]; then
+		tryopts="--enable-shared"
+	else
+		tryopts="--disable-shared"
+	fi
+
+	SAVE_CFLAGS="${CFLAGS}"
+	for tryopt in $tryopts __fail__; do
+		# Clean up, if needed
+		make distclean >/dev/null 2>/dev/null
+		rm -rf "${INSTDIR}"
+		mkdir "${INSTDIR}"
+
+		if [ "${tryopt}" = "__fail__" ]; then
+			exit 1
+		fi
+
+		if [ "${tryopt}" == "--enable-shared" ]; then
+			isshared="1"
+		else
+			isshared="0"
+		fi
+
+		# If build a static TclX for KitDLL, ensure that we use PIC
+		# so that it can be linked into the shared object
+		if [ "${isshared}" = "0" -a "${KITTARGET}" = "kitdll" ]; then
+			CFLAGS="${SAVE_CFLAGS} -fPIC"
+		else
+			CFLAGS="${SAVE_CFLAGS}"
+		fi
+		export CFLAGS
+
+		if [ "${isshared}" = '0' ]; then
+			sed 's@USE_TCL_STUBS@XXX_TCL_STUBS@g' configure > configure.new
+		else
+			sed 's@XXX_TCL_STUBS@USE_TCL_STUBS@g' configure > configure.new
+		fi
+		cat configure.new > configure
+		rm -f configure.new
+		chmod +x configure
+
+		(
+			echo "Running: ./configure $tryopt --prefix=\"${INSTDIR}\" --exec-prefix=\"${INSTDIR}\" --libdir=\"${INSTDIR}/lib\" --with-tcl=\"${TCLCONFIGDIR}\" ${CONFIGUREEXTRA}"
+			./configure $tryopt --prefix="${INSTDIR}" --exec-prefix="${INSTDIR}" --libdir="${INSTDIR}/lib" --with-tcl="${TCLCONFIGDIR}" ${CONFIGUREEXTRA}
+
+			echo "Running: ${MAKE:-make} tcllibdir=\"${INSTDIR}/lib\" AR=\"${AR:-ar}\" RANLIB=\"${RANLIB:-ranlib}\""
+			${MAKE:-make} tcllibdir="${INSTDIR}/lib" AR="${AR:-ar}" RANLIB="${RANLIB:-ranlib}" || exit 1
+
+			echo "Running: ${MAKE:-make} tcllibdir=\"${INSTDIR}/lib\" AR=\"${AR:-ar}\" RANLIB=\"${RANLIB:-ranlib}\" install"
+			${MAKE:-make} tcllibdir="${INSTDIR}/lib" AR="${AR:-ar}" RANLIB="${RANLIB:-ranlib}" install || exit 1
+		) || continue
+
+		break
+	done
+
+	# Install files needed by installation
+	pkgDir="${INSTDIR}/lib/tcc${TCLTCCVERS}"
+	pkgIndex="${pkgDir}/pkgIndex.tcl"
+
+	mkdir -p "${pkgDir}"
+
+	cp tcc.tcl "${pkgDir}"
+	cp pkgIndex.tcl "${pkgDir}"
+
+	cp -r "${INSTDIR}/lib" "${OUTDIR}" || exit 1
+	find "${OUTDIR}" -name '*.a' -type f | xargs -n 1 rm -f --
+
+	exit 0
+) || exit 1
+
+exit 0
