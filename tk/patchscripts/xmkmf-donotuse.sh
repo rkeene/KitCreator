e82374c348f02558c55ed3f371ee329b2250b7bc
rkeene <rkeene@noemail.net>
2014-05-27 14:20:39 +0000

    Added support for a manual knob (KC_CROSSCOMPILE) for determing whether fake xmkmf wrapper is used
    FossilOrigin-Name: f49c6300237a833abd4d77df8ef74adeabb6822a

--- tk/patchscripts/xmkmf-donotuse.sh
+++ tk/patchscripts/xmkmf-donotuse.sh
@@ -1,16 +1,26 @@
 #! /bin/bash
 
 # If we are not cross-compiling then don't worry about replacing "xmkmf" with a wrapper
-if [ -z "${CC}" ]; then
-	exit 0
-fi
-
-case "$(basename "${CC}")" in
-	*-*-*)
-		;;
-	*)
+if [ -z "${KC_CROSSCOMPILE}" ]; then
+	## If KC_CROSSCOMPILE was unspecified, try to autodetect
+	if [ -z "${CC}" ]; then
 		exit 0
-esac
+	fi
+
+	case "$(basename "${CC}")" in
+		*-*-*)
+			;;
+		*)
+			exit 0
+	esac
+else
+	## If KC_CROSSCOMPILE was specified as 0, we are not cross-compiling
+	if [ "${KC_CROSSCOMPILE}" = '0' ]; then
+		exit 0
+	fi
+
+	## Otherwise, we are cross-compiling
+fi
 
 # Create an "xmkmf" wrapper which exits in failure so that autoconf will try
 # to locate headers/libraries normally
