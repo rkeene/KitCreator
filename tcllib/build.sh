22862a3e59bcf5638d1528db4b5bc6d6cf47b031
rkeene <rkeene@noemail.net>
2016-09-06 04:43:22 +0000

    Upgraded to latest version of tcllib
    FossilOrigin-Name: 5ccf6eac7abf2776b35ecd12083ea00fff8e4504

--- tcllib/build.sh
+++ tcllib/build.sh
@@ -2,6 +2,6 @@
 
 # BuildCompatible: KitCreator
 
-version='1.16'
+version='1.18'
 url="http://sourceforge.net/projects/tcllib/files/tcllib/${version}/tcllib-${version}.tar.bz2"
-sha256='033334306d3ffc499c8830d393bf7528227e49c68077b3e9247c4171b15be012'
+sha256='73ee3067e20663e9401d0291028f9a5ffab8e71c7bbe1673c79edb5f266cfe7c'
