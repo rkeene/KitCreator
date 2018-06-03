164d741cae1b7b57c709dd82fc1b02a2f94eb9aa
rkeene <rkeene@noemail.net>
2018-06-03 17:38:49 +0000

    Added LMDB package
    FossilOrigin-Name: 3760a068802d15ec2b5cd1b9c5bc238855d2d65a

---
+++ lmdb/build.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# BuildCompatible: KitCreator
+
+version="0.3.6"
+url="https://github.com/ray2501/tcl-lmdb/archive/${version}.tar.gz"
+sha256='887f72e7c8bc569d74d3c9f151313eadeb19036b4bf8ebd68d7da45aa8a14da7'
