64f927841f7705f49f90c60c167e362255ee522c
rkeene <rkeene@noemail.net>
2018-10-25 00:14:50 +0000

    Upgrade to tDOM 0.9.1
    FossilOrigin-Name: cfdca57b2a4d69d7b891bef5783c8231de368eaa

--- tdom/build.sh
+++ tdom/build.sh
@@ -2,6 +2,6 @@
 
 # BuildCompatible: KitCreator
 
-version='0.8.3'
-url="https://github.com/downloads/tDOM/tdom/tDOM-${version}.tgz"
-sha256='496d5950907dd70db61bc20c55a05a5521363ce3c076e2fdd97270181bc720b9'
+version='0.9.1'
+url="http://tdom.org/downloads/tdom-${version}-src.tgz"
+sha256='8e89b0fff005250b1117e05b986c94f119f8524601753c05df18e9b6de58fb8f'
