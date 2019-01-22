8922add82905c439f66dd876a34961d2c5f6b1d3
rkeene <rkeene@noemail.net>
2019-01-22 22:52:08 +0000

    Slightly better handling of linkadd, may need to make this more generic in the future
    FossilOrigin-Name: 73ac42ec6a14b220829da4efcfd20411f2076829

--- lmdb/build.sh
+++ lmdb/build.sh
@@ -8,8 +8,27 @@ sha256='d19a19376da6716a1ed159a918e631030491f8b6a4ef9e72a4221481b24b2e40'
 
 function postinstall() {
 	local name
+	local isWindows
 
-	find "${installdir}" -type -f -name '*.a' | while IFS='' read -r name; do
-		echo '-lntdll' > "${name}.linkadd"
-	done
+	# Windows-only
+	isWindows='false'
+	if [ "${KC_CROSSCOMPILE}" = '1' ]; then
+		case "${KC_CROSSCOMPILE_HOST_OS}" in
+			*-cygwin|*-mingw32|*-mingw32-*|*-cygwin-*)
+				isWindows='true'
+				;;
+		esac
+	else
+		case "${OSTYPE}" in
+			msys|win*|cygwin)
+				isWindows='true'
+				;;
+		esac
+	fi
+
+	if [ "${isWindows}" = 'true' ]; then
+		find "${installdir}" -type -f -name '*.a' | while IFS='' read -r name; do
+			echo '-lntdll' > "${name}.linkadd"
+		done
+	fi
 }
