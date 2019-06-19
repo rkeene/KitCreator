67bc7035643b10cf5261233935beface8f4c0e47
rkeene <rkeene@noemail.net>
2019-06-19 18:36:13 +0000

    Deal with LIB files better
    FossilOrigin-Name: e46c4439b242aad8c4c2cc4266a8a38c626d2330

--- common/common.sh
+++ common/common.sh
@@ -276,8 +276,21 @@ function preinstall() {
 }
 
 function install() {
+	local filename newFilename
+
 	mkdir -p "${installdir}/lib" || return 1
 	${MAKE:-make} tcllibdir="${installdir}/lib" TCL_PACKAGE_PATH="${installdir}/lib" "${make_extra[@]}" install || return 1
+
+	# Rename ".LIB" files to ".a" files which KitCreator expects elsewhere
+	find "${installdir}/lib" -type f -iname '*.lib' -o -iname '*.lib.linkadd' | while IFS='' read -r filename; do
+		case "${filename}" in
+			*.[Dd][Ll][Ll].[Ll][Ii][Bb])
+				continue
+				;;
+		esac
+		newFilename="$(echo "${filename}" | sed 's@\.lib$@.a@i;s@\.lib\.linkadd$@.a.linkadd@')"
+		mv "${filename}" "${newFilename}"
+	done
 }
 
 function postinstall() {
