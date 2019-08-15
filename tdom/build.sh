ad8b114481dd1edb0861787ff31c68555ec05486
rkeene <rkeene@noemail.net>
2019-08-15 19:30:56 +0000

    Correct tDOM archive hash
    FossilOrigin-Name: 6e1940c326fc16ea18218556977470f1528f4eca

--- tdom/build.sh
+++ tdom/build.sh
@@ -4,4 +4,4 @@
 
 version='0.9.1'
 url="http://tdom.org/downloads/tdom-${version}-src.tgz"
-sha256='8e89b0fff005250b1117e05b986c94f119f8524601753c05df18e9b6de58fb8f'
+sha256='3b1f644cf07533fe4afaa8cb709cb00a899d9e9ebfa66f4674aa2dcfb398242c'
