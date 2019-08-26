734972653ff163f35e1bdefe19e25611a122bc48
rkeene <rkeene@noemail.net>
2019-08-26 14:27:08 +0000

    Ignore more parameters while building tcllux
    FossilOrigin-Name: f9d652be1428f5eaa345414d1f9a4d6acd8cb3e4

--- tcllux/build.sh
+++ tcllux/build.sh
@@ -5,7 +5,7 @@
 version='0.4'
 url="https://chiselapp.com/user/stwo/repository/tcllux/uv/tcllux-${version}.tar.gz"
 sha256='c4fecf6852b35089a8f6d0e1a6fe8feb70c5f7e9f4eb91ad6b4caa9c65c211fd'
-pkg_ignore_opts=(--enable-threads --disable-threads)
+pkg_ignore_opts=(--enable-threads --disable-threads --enable-kit-storage)
 pkg_no_support_for_static='1'
 
 function postinstall() {
