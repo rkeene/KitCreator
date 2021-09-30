531bdb550c28c14a26e8c2ea77039ac5569934e5
rkeene <rkeene@noemail.net>
2021-09-30 12:21:44 +0000

    Add Ruby Lane JSON package
    FossilOrigin-Name: e9f4a458352d4ed3fcf06d8ec4bdd1f17a60be70

---
+++ rl_json/build.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# BuildCompatible: KitCreator
+
+version='0.11.0'
+url="https://github.com/RubyLane/rl_json/archive/refs/tags/${version}.tar.gz"
+sha256='5f711c3196da5acec01bb24a76404a7f25f5f2f4b939aaa874bd7a7e94e482e1'
