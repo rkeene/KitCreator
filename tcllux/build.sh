cf74baf6ca4336de572f29ae4b539486d5ee9f07
rkeene <rkeene@noemail.net>
2022-08-12 15:21:39 +0000

    Remove setting _USE_32BIT_TIME_T, does not seem to be required anymore.
    It was added as part of [cb7ed6affe], but now causes [df2dd68dca]
    
    FossilOrigin-Name: b93594845ddef03e5d080e745a4dc535013748af

--- tcllux/build.sh
+++ tcllux/build.sh
@@ -2,13 +2,13 @@
 
 # BuildCompatible: KitCreator
 
-version='0.4'
+version='0.5'
 url="https://chiselapp.com/user/stwo/repository/tcllux/uv/tcllux-${version}.tar.gz"
-sha256='c4fecf6852b35089a8f6d0e1a6fe8feb70c5f7e9f4eb91ad6b4caa9c65c211fd'
+sha256='c5100f784b0790f878f75dbdadc109f69e3cee536eb9376785a360284345d4fe'
 pkg_ignore_opts=(--enable-threads --disable-threads --enable-kit-storage)
 pkg_no_support_for_static='1'
 
-function postinstall() {
+function postinstall_() {
 	(
 		cd "${installdir}/lib" || exit 1
 		if [ -d 'tcl' ]; then
